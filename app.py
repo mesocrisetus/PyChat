67f2af5a3346d7fe701938a403bf7773aa76220e
Mesocrisetus <alexander.bolivardiaz@gmail.com>
2025-01-15 12:29:04 -0500

    Chat functions added, database modified

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template,url_for,request,redirect,flash
 import db_modules as dbModules
 import os
+from datetime import datetime
 
 #App Config
 app = Flask(__name__)
@@ -21,7 +22,10 @@ def loginIn():
     loginCheck = dbModules.login(email,password)
 
     if loginCheck:
-        return render_template('index.html')
+        userId = loginCheck[0]
+        users = dbModules.getAllUsers()
+        users = [user for user in users if user[1] != email]
+        return render_template('index.html',email=email,userId=userId,users=users)
 
     else:
         pass
@@ -62,13 +66,38 @@ def register():
             message = "El usuario se ha registrado correctamente"
             flash(message)
             return render_template('login.html')
+        
+
+@app.route('/chat/<int:userId>')
+def chat(userId):    
+    user2Id = request.args.get('user2')
+    room = dbModules.get_room(userId, user2Id) 
+    if not room:
+        # Si la sala no existe, crearla
+        room_id = dbModules.create_room(userId, user2Id)
+    else:
+        room_id = room[0]
+    messages = dbModules.get_messages(room_id)
+    print(messages)
+    users = dbModules.getAllUsers()
+
+    return render_template('index.html', roomId=room_id, messages=messages, userId=user2Id,users=users)
+
+@app.route('/sendMessage', methods=['POST'])
+def send_message():
+    url = request.form.get('current_url')
+    room_id = request.form['roomId']
+    user_id = request.form['userId']
+    message_text = request.form['chatInput']
     
+    # Obtener la fecha y hora actual    
+    message_date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
+    # Insertar el mensaje en la base de datos
+    dbModules.send_message(user_id, room_id, message_text, message_date)
 
-
-
-
-
+    # Redirigir de nuevo a la página de chat con los mensajes actualizados
+    return redirect(url)
 
 
 
--- db_modules.py
+++ db_modules.py
@@ -1,10 +1,11 @@
 import sqlite3
 
-# Conexión global
+# Global connection
 connection = sqlite3.connect("database/pychat.db", check_same_thread=False)
 cursor = connection.cursor()
 
 
+#Login and register
 
 def userExist(email):
     sql = "SELECT 1 FROM users WHERE email = '%s' LIMIT 1;"%(email)
@@ -23,16 +24,13 @@ def register(email,password):
         return True
     
     except sqlite3.Error as e:
-        print("Operation Error:",e)
-
-
-    
+        print("Operation Error:",e)    
 
 
 def login(email,password):
     try:
         cursor = connection.cursor()
-        sql = "SELECT email, password FROM users WHERE email=? AND password=?"
+        sql = "SELECT id, email, password FROM users WHERE email=? AND password=?"
         cursor.execute(sql, (email, password))
         result = cursor.fetchall()
         return result
@@ -43,3 +41,62 @@ def login(email,password):
 
     connection.commit()
     cursor.close()
+
+# Get All users
+
+def getAllUsers():
+    try:
+        sql = "SELECT * FROM users"
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        return result
+    
+    except sqlite3.Error as e:
+        print("Operation Error:",e)   
+
+
+#chat Exist
+def get_room(user1_id, user2_id):
+    query = """
+        SELECT r.id
+        FROM room r
+        JOIN roomUser ru1 ON r.id = ru1.roomId
+        JOIN roomUser ru2 ON r.id = ru2.roomId
+        WHERE ru1.userId = ? AND ru2.userId = ?
+    """
+    cursor.execute(query, (user1_id, user2_id))
+    room = cursor.fetchone()
+    return room
+
+def create_room(user1_id, user2_id):
+    query = "INSERT INTO room (idType, time) VALUES (?, ?)"
+    cursor.execute(query, (1, '2025-01-15 12:00:00'))  # idType=1 para "private"
+    connection.commit()
+    
+    room_id = cursor.lastrowid  # Obtener el id de la sala recién creada
+    
+    # Agregar los usuarios a la sala
+    query = "INSERT INTO roomUser (roomId, userId) VALUES (?, ?)"
+    cursor.executemany(query, [(room_id, user1_id), (room_id, user2_id)])
+    connection.commit()
+    
+    return room_id
+
+def get_messages(room_id):
+    # Obtener los mensajes de la base de datos para la sala específica
+    cursor.execute("SELECT idUser, message, date FROM messages WHERE idRoom = ?", (room_id,))
+    messages = cursor.fetchall()
+    print(messages)
+    return messages
+
+
+def send_message(user_id, room_id, message, date):
+    query = """
+        INSERT INTO messages (idUser, idRoom, message, date)
+        VALUES (?, ?, ?, ?)
+    """
+    cursor.execute(query, (user_id, room_id, message, date))
+    connection.commit()
+
+get_messages(1)
+
